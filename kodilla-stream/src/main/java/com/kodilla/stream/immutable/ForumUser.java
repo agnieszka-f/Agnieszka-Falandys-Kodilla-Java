5d6d109456b46e1c284d8f57d4c2b51303834316
agnieszka-f <agnfal@gmail.com>
2020-06-20 14:48:32 +0200

    Moduł 7 - zad.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.immutable;
+
+public final class ForumUser{
+    private final String username;
+    private final String realname;
+
+    public ForumUser(final String username, final String realname){
+        this.username = username;
+        this.realname = realname;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getRealname(){
+        return this.realname;
+    }
+}
