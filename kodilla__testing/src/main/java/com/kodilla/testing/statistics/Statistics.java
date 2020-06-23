c1e06a29edcd1a16b3d35d7da3d68b8436541779
agnieszka-f <agnfal@gmail.com>
2020-06-23 20:35:06 +0200

    zadanie 6.5

---
+++ kodilla__testing/src/main/java/com/kodilla/testing/statistics/Statistics.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.statistics;
+
+import java.util.*;
+
+public interface Statistics {
+    public List<String> usersNames();
+    public int postsCount();
+    public int commentsCount();
+}
