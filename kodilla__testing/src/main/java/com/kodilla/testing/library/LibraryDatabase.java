830573bff8c4f270d7d76a8a2ff48f09c2840436
agnieszka-f <agnfal@gmail.com>
2020-06-26 16:22:29 +0200

    poprawa zad. 6.6

--- kodilla__testing/src/main/java/com/kodilla/testing/library/LibraryDatabase.java
+++ kodilla__testing/src/main/java/com/kodilla/testing/library/LibraryDatabase.java
@@ -5,6 +5,4 @@ import java.util.*;
 interface LibraryDatabase{
     List<Book> listBooksWithCondition(String titleFragment);
     List<Book> listBooksInHandsOf(LibraryUser libraryUser);
-    //boolean rentABook(LibraryUser libraryUser, Book book);
-    //int returnBooks(LibraryUser libraryUser);
 }
