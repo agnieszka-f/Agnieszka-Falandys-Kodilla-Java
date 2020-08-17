e7aa988bde8c420f626b463eb3821af0c17ac192
AgaF <agnfal@gmail.com>
2020-08-17 17:34:13 +0200

    TEST B

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//B
 public class KodillaCourseApplication {
 
     public static void main(String[] args) {
