eaef0baae291f8cdbad8b0b2241299a87f08260f
Agnieszka <agnfal@gmail.com>
2020-12-15 14:18:25 +0100

    poprawa

--- kodilla__testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuit.java
+++ kodilla__testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuit.java
@@ -24,29 +24,33 @@ public class CollectionTestSuit {
     }
     @Test
     public void testOddNumbersExterminatorEmptyList(){
+        //When
+        list2 = collection.exterminate(list1);
+        //Then
+        Assert.assertEquals(0,list2.size());
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
         list1.add(8);
         list1.add(2);
         list1.add(5);
         list1.add(9);
         list1.add(4);
-
+        //When
         list2 = collection.exterminate(list1);
 
-        if(list2.size() != 0){
-            System.out.println("Lista zawiera elementy. Liczba elementów: " + list2.size());
-            testOddNumbersExterminatorNormalList();
-        } else {
-            System.out.println("Lista jest pusta - błąd brak danych");
-        }
-    }
-    public void testOddNumbersExterminatorNormalList(){
-
+        int result = 0;
         for(Integer i: list2){
             if(i % 2 == 0){
                 System.out.println("Element parzysty: " + i);
             } else{
                 System.out.println("Znaleziono element nieprzysty: " + i);
+                result++;
             }
         }
+        //Then
+        Assert.assertEquals(0,result);
+        Assert.assertEquals(3,list2.size());
     }
 }
